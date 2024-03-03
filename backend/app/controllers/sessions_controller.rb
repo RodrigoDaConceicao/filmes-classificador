5da77f447487f089764df84da7598ebe978ea987
rodrigovoks <rodrigoqsconceicao@hotmail.com>
2024-03-03 20:42:50 -0300

    Chamadas de login em formato json

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,15 +1,27 @@
 class SessionsController < ApplicationController
   def new
+    respond_to do |format|
+      format.html
+      format.json { render json: {:authenticity_token => form_authenticity_token}.to_json }
+    end
   end
 
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to movies_path
+      respond_to do |format|
+        format.html { redirect_to movies_path }
+        format.json { render json: user.slice(:id,:username,:email,:admin).to_json}
+      end
     else
-      flash.now[:alert] = "Invalid email or password"
-      render :new
+      respond_to do |format|
+        format.html {
+          flash.now[:alert] = "Invalid email or password"
+          render :new
+        }
+        format.json { render json: {:message=>"Invalid email or password"}.to_json, status: 401}
+      end
     end
   end
 
